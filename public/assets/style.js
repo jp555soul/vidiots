917acafe39ed5b813eda30532bd7d155d4864373
Jason Paul <jmpaul@jasons-mbp.lan>
2019-06-10 09:12:45 -0700

    added paypal btn

--- public/assets/style.js
+++ public/assets/style.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	$(".push").sticky({bottomSpacing:0});
+	// $(".email-form").on('submit', function (e) {
+	// 	e.preventDefault();
+	// 	$.ajax({
+	// 		type: 'post',
+	// 		url: 'post.php',
+	// 		data: $('form').serialize(),
+	// 		success: function () {
+	// 	  		alert('form was submitted');
+	// 		}
+	// 	});
+	// });
 });
